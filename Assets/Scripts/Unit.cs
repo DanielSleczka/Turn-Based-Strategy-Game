69f5242d45a3fc5900491f1236c09ef3d3440a5c
DanielloGame <daniiu389@gmail.com>
2023-04-02 12:22:30 +0200

    add unit scripts and settings for units

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,31 +1,47 @@
+using Mono.Cecil;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
     private Vector3 targetPosition;
+
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
 
     private void Update()
     {
+
         float stoppingDistance = 0.02f;
         if(Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+            
+
+            unitAnimator.SetBool("IsWalking", true);
+        
         }
-
-
-        if(Input.GetMouseButtonDown(0))
+        else
         {
-            Move(MouseWorld.GetPosition());
+            unitAnimator.SetBool("IsWalking", false);
         }
+
     }
 
 
-    private void Move(Vector3 targetPosition)
+    public void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
     }
 }
+ 
